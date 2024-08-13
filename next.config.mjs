76b7016d806ba3c23f430fb185d406a09a34c6a3
mrmrtmrn <mrmrtmrn@gmail.com>
2024-08-13 21:06:39 +0900

    add next.config for images path prefix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   distDir: 'docs',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+  images: {
+    loader: 'imgix',
+    path: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
+  },
 };
 
 export default nextConfig;
