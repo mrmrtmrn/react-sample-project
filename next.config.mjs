8d1bbac8e4a7714875e48f9b7d60e2f3fd860b96
mrmrtmrn <mrmrtmrn@gmail.com>
2024-08-13 10:10:17 +0900

    update prod env

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   distDir: 'docs',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
 };
 
 export default nextConfig;
