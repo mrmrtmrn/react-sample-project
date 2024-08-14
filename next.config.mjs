fed3b4a43f7a579e818a3c71e17a841870476c9d
mrmrtmrn <mrmrtmrn@gmail.com>
2024-08-14 19:43:27 +0900

    use env

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: 'export',
   distDir: 'docs',
-  basePath: process.env.NODE_ENV === 'production' ? '/react-sample-project' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/react-sample-project/' : '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
 };
 
 export default nextConfig;
